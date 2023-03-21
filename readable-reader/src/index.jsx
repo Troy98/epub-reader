737024af662bdefdeb3df8c4fca8875805779f93
Güven Ciddioglu <Gauvun@hotmail.com>
2023-03-21 21:11:28 +0100

    Update index.jsx

--- readable-reader/src/index.jsx
+++ readable-reader/src/index.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+
+import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
+
 import Store from './store/store';
+
 import App from './App';
+
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+
+const container = document.getElementById('root');
+
+
+
+ReactDOM.render(
+
   <Provider store={Store}>
+
     <App />
+
   </Provider>,
+
+  container,
+
 );
